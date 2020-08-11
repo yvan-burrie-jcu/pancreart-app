618a1856db17a3669348485083b425ce50ffd383
Yvan Burrie <yvan.burrie@hotmail.com>
2020-08-12 00:21:12 +1000

    Create mobile project with empty activity

---
+++ app/src/test/java/jcu/cp3407/pancreart/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package jcu.cp3407.pancreart;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
